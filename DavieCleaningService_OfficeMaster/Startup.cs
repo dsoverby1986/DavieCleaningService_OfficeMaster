cde2cbf4e7426bf1c59a61761da5a84efbe39562
Shane Overby <dsoverby1986@gmail.com>
2015-11-05 20:47:29 -0500

    Created. Template. Home, About, Contact.
    Created Project. Pulled in template. Home page nearly complete. About
    page laid out with some content. Contact page linked in navbar.

---
+++ DavieCleaningService_OfficeMaster/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DavieCleaningService_OfficeMaster.Startup))]
+namespace DavieCleaningService_OfficeMaster
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
